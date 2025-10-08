7a13188f5fad21ba58bb545cbca961dcfa884447
vishnu-0111-vlsi <jammunaboyinavishnuvardhan@gmail.com>
2025-10-08 07:47:18 +0530

    Create fifo_pkg.sv

---
+++ src/fifo_pkg.sv
@@ -0,0 +1,14 @@
+package pkg;
+        import uvm_pkg::*;
+        `include "uvm_macros.svh"
+        `include "seq_item.sv"
+        `include "sequencer.sv"
+        `include "sequence.sv"
+        `include "driver.sv"
+        `include "monitor.sv"
+        `include "agent.sv"
+        `include "scoreboard.sv"
+		`include "subscriber.sv"
+        `include "environment.sv"
+        `include "test.sv"
+endpackage
